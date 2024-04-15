4145103d13accf7fb6fb27f440453e38d86e90f2
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2024-04-15 09:03:25 +0200

    implemented code generation and storage

--- webserver/app.py
+++ webserver/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, make_response
+from random import choice
+from string import ascii_uppercase
+from datetime import datetime, timedelta
+from dataclasses import dataclass, field
 
 app = Flask(__name__, template_folder="templates")
 
-class Location:
-    def __init__(self, name, max_capacity: int, availability=None):
-        self.name = name
-        self.max_capacity = max_capacity
-        self.availability = availability if availability else max_capacity
-        self.reservations = {}
+@dataclass
+class Reservation:
+    code: str
+    expiry: datetime
 
-locations = {1: Location('Elgeseter', 3), 5: Location('Solsiden', 10)}
+@dataclass
+class Location:
+    name: str
+    max_capacity: int
+    availability: int
+    reservations: "dict[str: Reservation]" = field(default_factory=lambda: dict())
+
+locations = {1: Location('Elgeseter', 3, 3), 5: Location('Solsiden', 10, 10)}
 
 @app.route("/locations")
 @app.route("/locations/<int:location_id>")
@@ -19,15 +28,42 @@ def locations_handler(location_id=None):
 
     return render_template("locations.html", locations=locations)
 
-@app.route("/locations/<int:location_id>/reservations", methods=['GET', 'POST', 'DELETE'])
+@app.route("/locations/<int:location_id>/reservations", methods=['GET'])
 def reservations_handler(location_id):
+
     if request.method == 'GET':
         if location_id not in locations:
             return f"Invalid location id", 404
         
+        reservation = None
         location = locations[location_id]
-        return f"Availability {location.availability}/{location.max_capacity}"
+        print(location.reservations)
+        if 'CODE' in request.cookies:
+            code = request.cookies.get('CODE')
+            reservation = location.reservations.get(code)
+            print(reservation)
 
+        return render_template("reservations.html", location=location, reservation=reservation)
 
     elif request.method == "POST":
         print("POST REQUEST")
+
+@app.route("/locations/<int:location_id>/reservations/generate", methods=['GET'])
+def generatation_handler(location_id):
+    if location_id not in locations:
+        return f"Invalid location id", 404
+    
+    location = locations[location_id]
+
+    while True:
+        code = "".join([choice(ascii_uppercase) for _ in range(4)])
+        if code not in location.reservations:
+            break
+    
+    location.reservations[code] = Reservation(code, datetime.now() + timedelta(minutes=15))
+
+    response = make_response(code)
+    response.set_cookie("CODE", code, expires=datetime.now() + timedelta(hours=24, minutes=15))
+    return response
+
+
