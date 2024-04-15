45ee50d4bda030db62feef9336fd6c2109bd5c33
Fabian <fabbefab@gmail.com>
2024-04-15 15:19:28 +0200

    Started a new document on the statemachines for the charger

---
+++ charger/Electric_charger/statemachinefabian.py
@@ -0,0 +1,69 @@
+from stmpy import Machine, Driver
+
+import ipywidgets as widgets
+from IPython.display import display
+
+
+class Charger:
+    # Send a message to start charging
+    def on_button_start(self, b_start):
+        self.stm.send("start_charging")
+
+    def on_button_reserve(self, b_reserver):
+        self.stm.send("reserve_charger")
+
+
+    def __init__(self):
+
+        # Buttons
+        self.button_start = widgets.Button(description="Start charging")
+        self.button_reserve = widgets.Button(description="Reserve charger")
+        
+        # Text field to display states
+        self.text = widgets.Text(value='', placeholder='', description='String:', disabled = False)
+        display(self.text, self.button_start, self.button_reserve)
+
+
+    def available():
+        test
+
+    def reserved():
+        test
+
+    def ban_user():
+        test
+
+    def charging():
+        test
+    
+    def charging_battery_full():
+        test
+
+    def overcharge():
+        test
+
+    def finish_charging():
+        test
+
+charger = Charger()
+#Initial transitiion
+t0 = {'source':'initial',
+      'target':'spinning'}
+
+#Transition from available to reserved
+t1 = {'trigger':'reserve_charger',
+      'source':'available',
+      'target':'reserved'}
+
+#Transition from availble to charging
+t2 = {'trigger': 'start_charging',
+      'source':'available',
+      'target':'charging'}
+
+#Transition from  
+
+
+
+# STATES
+
+machine = Machine(name="charger")
