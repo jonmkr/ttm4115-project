5285b9caccdf5e7fd9c84bd8281f447d01df41f8
Fabian <fabbefab@gmail.com>
2024-04-15 15:34:46 +0200

    added code on statmachinefabian

--- charger/Electric_charger/statemachinefabian.py
+++ charger/Electric_charger/statemachinefabian.py
@@ -9,8 +9,12 @@ class Charger:
     def on_button_start(self, b_start):
         self.stm.send("start_charging")
 
-    def on_button_reserve(self, b_reserver):
+    def on_button_reserve(self, b_reserve):
         self.stm.send("reserve_charger")
+
+    def on_button_stop(self, b_stop):
+        self.stm.send("stop_charger")
+
 
 
     def __init__(self):
@@ -24,25 +28,25 @@ class Charger:
         display(self.text, self.button_start, self.button_reserve)
 
 
-    def available():
+    def available(self):
         test = test
 
-    def reserved():
+    def reserved(self):
         test = test
 
-    def ban_user():
+    def ban_user(self):
         test = test
 
-    def charging():
+    def charging(self):
         test = test
     
-    def charging_battery_full():
+    def charging_battery_full(self):
         test = test
 
-    def overcharge():
+    def overcharge(self):
         test = test
 
-    def finish_charging():
+    def finish_charging(self):
         test = test
 
 charger = Charger()
@@ -55,12 +59,34 @@ t1 = {'trigger':'reserve_charger',
       'source':'available',
       'target':'reserved'}
 
+#Transition from reserved to charging
+
+t2 = {'trigger':'start_charging',
+      'source':'reserved',
+      'target':'ban_user'}
+
 #Transition from availble to charging
-t2 = {'trigger': 'start_charging',
+t3 = {'trigger': 'start_charging',
       'source':'available',
       'target':'charging'}
 
-#Transition from  
+#Transition from charging to finish_charging
+t4 = {}
+
+#Transition from charging to charging_battery_full
+t5 = {}
+
+#Transition from charging_battery_full to finish_charging
+t6 = {}
+
+#Transition from charging_battery_full to overcharge
+t7 = {}
+
+#Transition from finish_charging to available
+t8 = {}
+
+
+
 
 
 
