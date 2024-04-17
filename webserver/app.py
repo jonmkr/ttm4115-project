6da91713ee39634f34fbffb530fa33cc674c6882
Jon Martin Kristiansen <jonmkr@stud.ntnu.no>
2024-04-17 08:30:20 +0200

    added reservation storage and management

--- webserver/app.py
+++ webserver/app.py
@@ -1,15 +1,43 @@
-from flask import Flask, request, redirect, render_template, make_response
+import json
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+from queue import Queue
 from random import choice
 from string import ascii_uppercase
-from datetime import datetime, timedelta
-from dataclasses import dataclass, field
+from time import sleep
+
+from flask import Flask, make_response, redirect, render_template, request
+from flask_sock import Sock
+from reservation_stm import states, transitions
+from stmpy import Driver, Machine
+
+input_queue = Queue()
+output_queue = Queue()
 
 app = Flask(__name__, template_folder="templates")
+sock = Sock(app)
 
-@dataclass
+users = dict()
+
 class Reservation:
-    code: str
-    expiry: datetime
+    def __init__(self, code, expiry):
+        self.code = code
+        self.expiry = expiry
+        self.expired = False
+        self.dangling = False
+
+        self.stm = Machine('reservation', transitions, self, states)
+
+        output_queue.put(json.dumps({'type': 1, 'code': self.code}))
+
+    def reservation_timeout(self):
+        print("Reservation", self.code, "expired")
+        self.expired = True
+        output_queue.put(json.dumps({'type': 2, 'code': self.code}))
+
+    def invalidate_reservation(self):
+        print("Reservation", self.code, "set for removal")
+        self.dangling = True
 
 @dataclass
 class Location:
@@ -18,8 +46,31 @@ class Location:
     availability: int
     reservations: "dict[str: Reservation]" = field(default_factory=lambda: dict())
 
-locations = {1: Location('Elgeseter', 3, 3), 5: Location('Solsiden', 10, 10)}
+locations = {}
 
+driver = Driver()
+driver.start(keep_active=True)
+
+
+@sock.route("/ws")
+def websocket(ws):
+    data = ws.receive()
+    payload = json.loads(data)
+
+    location_id = payload.pop('id')
+    locations[location_id] = Location(payload['name'], payload['max_capacity'], payload['availability'])
+
+    print(payload['name'] + " added")
+
+    while True:
+        if not output_queue.empty():
+            ws.send(json.dumps(output_queue.get()))
+
+        recv = ws.receive(timeout=1)
+        if recv is not None:
+            input_queue.put(json.loads(recv))
+
+@app.route("/")
 @app.route("/locations")
 @app.route("/locations/<int:location_id>")
 def locations_handler(location_id=None):
@@ -30,23 +81,40 @@ def locations_handler(location_id=None):
 
 @app.route("/locations/<int:location_id>/reservations", methods=['GET'])
 def reservations_handler(location_id):
-
     if request.method == 'GET':
         if location_id not in locations:
             return f"Invalid location id", 404
-        
-        reservation = None
+
         location = locations[location_id]
-        print(location.reservations)
+
+        reservation_count = 0
+        for code, reservation in location.reservations.items():
+            if not reservation.expired:
+                reservation_count += 1
+
+            if reservation.dangling:
+                reservation.stm.terminate()
+                del location.reservations[code]
+
+        reservation = None
+        
+        print(request.cookies)
         if 'CODE' in request.cookies:
             code = request.cookies.get('CODE')
-            reservation = location.reservations.get(code)
-            print(reservation)
 
-        return render_template("reservations.html", location=location, reservation=reservation)
+            if code in location.reservations:
+                reservation = location.reservations[code]
 
-    elif request.method == "POST":
-        print("POST REQUEST")
+        print("Reservation:", reservation)
+        
+        template = render_template("reservations.html", location=location, reservation_count=reservation_count, reservation=reservation)
+
+        response = make_response(template)
+
+        if reservation is None:
+            response.set_cookie("CODE", "", max_age=0)
+
+        return response
 
 @app.route("/locations/<int:location_id>/reservations/generate", methods=['GET'])
 def generatation_handler(location_id):
@@ -60,10 +128,17 @@ def generatation_handler(location_id):
         if code not in location.reservations:
             break
     
-    location.reservations[code] = Reservation(code, datetime.now() + timedelta(minutes=15))
+    expiry = datetime.now() + timedelta(minutes=15)
 
-    response = make_response(code)
-    response.set_cookie("CODE", code, expires=datetime.now() + timedelta(hours=24, minutes=15))
+    reservation = Reservation(code, expiry)
+    location.reservations[code] = reservation
+    driver.add_machine(reservation.stm)
+
+    reservation.stm.send('Reserve')
+
+    response = make_response()
+    response.set_cookie("CODE", code, expires=expiry + timedelta(hours=24))
     return response
 
-
+if __name__ == "__main__":
+    app.run()
